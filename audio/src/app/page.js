c4252fb195c8b735076546808c8bc0a66da847aa
NightWing2099 <62215488+KidFlash2099@users.noreply.github.com>
2023-10-05 23:40:08 -0400

    adding images to link section working on the next image section after this

--- audio/src/app/page.js
+++ audio/src/app/page.js
@@ -41,12 +41,25 @@ export default function Home() {
         <div className="link-container row">
           <div className="link">
             <div className="shop-link">
+
+            <Image 
+            className="headphone-image" src="/image-xx99-mark-one-headphones.png" 
+            height={150}
+            width={150}
+            alt=" headphones link" />
             <h1>headphones</h1>
               <a className="quick-link"><h2>Shop <span>&gt;</span></h2></a>
             </div>
           </div>
           <div className="link">
             <div className="shop-link">
+            <Image 
+              className="speaker-image"
+              src="/image-zx9-speaker.png"
+              height={150}
+              width={150}
+              alt="speaker link photo"
+            />
             <h1>speakers</h1>
               <a className="quick-link"><h2>Shop <span>&gt;</span></h2></a>
             </div>
