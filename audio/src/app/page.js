dd6d6adba026a2bd968ec51c1fb484fd0490e811
NightWing2099 <62215488+KidFlash2099@users.noreply.github.com>
2023-10-07 18:23:27 -0400

    adding styles to next section of the first page

--- audio/src/app/page.js
+++ audio/src/app/page.js
@@ -41,36 +41,44 @@ export default function Home() {
         <div className="link-container row">
           <div className="link">
             <div className="shop-link">
-
-            <Image 
-            className="headphone-image" src="/image-xx99-mark-one-headphones.png" 
-            height={150}
-            width={150}
-            alt=" headphones link" />
-            <h1>headphones</h1>
-              <a className="quick-link"><h2>Shop <span>&gt;</span></h2></a>
-            </div>
-          </div>
-          <div className="link">
-            <div className="shop-link">
-            <Image 
-              className="speaker-image"
-              src="/image-zx9-speaker.png"
+              <Image 
+              className="headphone-image" src="/image-xx99-mark-one-headphones.png" 
               height={150}
               width={150}
-              alt="speaker link photo"
-            />
-            <h1>speakers</h1>
-              <a className="quick-link"><h2>Shop <span>&gt;</span></h2></a>
+              alt=" headphones link"
+              />
+              <h1>headphones</h1>
+                <a className="quick-link"><h2>Shop <span>&gt;</span></h2></a>
             </div>
           </div>
           <div className="link">
             <div className="shop-link">
-            <h1>earphones</h1>
-              <a className="quick-link"><h2>Shop <span>&gt;</span></h2></a>
+              <Image 
+                className="speaker-image"
+                src="/image-zx9-speaker.png"
+                height={150}
+                width={150}
+                alt="speaker link photo"
+              />
+              <h1>speakers</h1>
+                <a className="quick-link"><h2>Shop <span>&gt;</span></h2></a>
+            </div>
+          </div>
+          <div className="link">
+            <div className="shop-link">
+              <Image 
+                className="earphones-image"
+                src="/image-yx1-earphones.png"
+                height={150}
+                width={150}
+                alt="speaker link photo"
+              />
+              <h1>earphones</h1>
+                <a className="quick-link"><h2>Shop <span>&gt;</span></h2></a>
             </div>
           </div>
         </div>
+        <div className="speaker-container"></div>
       </div>
     </body>
   )
