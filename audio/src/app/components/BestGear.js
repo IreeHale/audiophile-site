18bbe7c81c7ddee5bd01ae30e7984483f8f8d8fc
NightWing2099 <62215488+KidFlash2099@users.noreply.github.com>
2023-10-14 04:29:57 -0400

    adding components to project and will be working on other pages soon

---
+++ audio/src/app/components/BestGear.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Image from 'next/image'
+
+
+export default function BestGear() {
+  return (
+        <div className="mark2-headphones">
+            <div className="mark2-info">
+                <h1>bringing you the <span>best</span> audio gear</h1>
+                <p>
+                Bringing you the best audio gear
+                Located at the heart of New York City, Audiophile is the premier store for high end headphones, 
+                earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration 
+                rooms available for you to browse and experience a wide range of our products. Stop by our store 
+                to meet some of the fantastic people who make Audiophile the best place to buy your portable 
+                audio equipment.
+                </p>
+            </div>
+            <Image
+                className="best-gear"
+                src="/image-best-gear.jpg"
+                height={580}
+                width={520}
+            />
+        </div>
+  )
+}
