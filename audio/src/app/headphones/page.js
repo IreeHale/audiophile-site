b1b935e80f8e666847f3c846c8732ea3c478f328
NightWing2099 <62215488+KidFlash2099@users.noreply.github.com>
2023-10-15 17:25:56 -0400

    adding info section for headphone page

--- audio/src/app/headphones/page.js
+++ audio/src/app/headphones/page.js
@@ -15,15 +15,46 @@ export default function headphones() {
             <h1>headphones</h1>
         </div>
         <div className="container-two">
-        <div className="studio2-headphones">
-        <div className="studio2-image"></div>
-            <div className="studio2-info">
-                <h2>new product</h2>
-                <h1>xx99 mark II headphones</h1>
-                <p>The new XX99 Mark II is the pinnacle of pristine audio. It redefines your premium headphones experience by reproducing the balanced depth and precision of studio-quality sound.</p>
-                <button>see product</button>
+            <div className="studio2-headphones">
+                <div className="studio2-image"></div>
+                    <div className="studio2-info">
+                        <h2>new product</h2>
+                        <h1>xx99 mark II headphones</h1>
+                        <p>
+                            The new XX99 Mark II is the pinnacle of pristine audio. 
+                            It redefines your premium headphones experience by reproducing 
+                            the balanced depth and precision of studio-quality sound.
+                        </p>
+                        <button>see product</button>
+                    </div>
+                </div>
+            <div className="studio1-headphones">
+                <div className="studio1-info">
+                    <h1>xx99 mark I headphones</h1>
+                    <p>
+                        As the gold for headphones, 
+                        the classic xx99 Mark I offers detailed and accurate 
+                        audio reproduction for audiophiles, mixing engineers, and music aficionados 
+                        alike in studios and on the go.
+                    </p>
+                    <button>see product</button>
+                </div>
+                <div className="studio1-image"></div>
             </div>
-        </div>
+            <div className="studio-headphones">
+                <div className="studio-image"></div>
+                    <div className="studio-info">
+                        <h2>new product</h2>
+                        <h1>xx99 mark II headphones</h1>
+                        <p>
+                            The new XX99 Mark II is the pinnacle of pristine audio. 
+                            It redefines your premium headphones experience by reproducing 
+                            the balanced depth and precision of studio-quality sound.
+                        </p>
+                        <button>see product</button>
+                    </div>
+                </div>
+            </div>
             <ShopLinks />
             <BestGear />
             <Footer />
