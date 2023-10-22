1a7793b7e908133534612ac6359c93938e54139e
NightWing2099 <62215488+KidFlash2099@users.noreply.github.com>
2023-10-22 01:53:30 -0400

    adding styles to page removing overfolw from page and adding text and pictures

--- audio/src/app/speakers/page.js
+++ audio/src/app/speakers/page.js
@@ -1,9 +1,50 @@
 import React from 'react'
+import './styles.modules.scss'
+import Navbar from '../components/Navbar'
+import ShopLinks from '../components/ShopLinks'
+import BestGear from '../components/BestGear'
+import Footer from '../components/Footer'
+
+
 
 export default function speakers() {
   return (
-    <div>
-        <h1>hello world</h1>
-    </div>
+    <body>
+      <div className="container-one">
+            <Navbar />
+            <h1>speakers</h1>
+        </div>
+        <div className="container-two">
+                <div className="studio2-headphones">
+                  <div className="studio2-image"></div>
+                      <div className="studio2-info">
+                          <h2>new product</h2>
+                          <h1>zx9 speaker</h1>
+                          <p>
+                            Upgrade your sound system with the all new ZX9 active
+                            speaker. It's a bookshelf speaker system that offers
+                            truly wireless connectivity - creating new possibilities for
+                            more pleasing and practical audio setups.
+                          </p>
+                          <button>see product</button>
+                      </div>
+                  </div>
+                  <div className="studio1-headphones">
+                    <div className="studio1-info">
+                      <h1>zx7 speaker</h1>
+                      <p>
+                          Stream high quality sound wirelessly with minimal loss. The ZX7
+                          bookshelf speaker uses high-end audiophile components that
+                          represents the top of the line powered speakers for home or studio use.
+                      </p>
+                      <button>see product</button>
+                    </div>
+                  <div className="studio1-image"></div>
+                </div>
+                <ShopLinks />
+                <BestGear />
+                <Footer />
+      </div>
+    </body>
   )
 }
