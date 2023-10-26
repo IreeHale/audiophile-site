8e64d37e23b3b35928fc8131be35f777eb060a58
NightWing2099 <62215488+KidFlash2099@users.noreply.github.com>
2023-10-26 01:28:15 -0400

    adding next page will work on functionality later

---
+++ audio/src/app/product-details-headphones/page.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './styles1.modules.scss'
+
+
+//Components
+import Navbar from '../components/Navbar'
+
+export default function details() {
+  return (
+    <body>
+        <div className="container-one">
+            <Navbar />
+        </div>
+        <div className="container-two">
+            <div className="studio2-headphones">
+                <div className="studio2-image"></div>
+                    <div className="studio2-info">
+                        <h2>new product</h2>
+                        <h1>xx99 mark II headphones</h1>
+                        <p>
+                            The new XX99 Mark II is the pinnacle of pristine audio. 
+                            It redefines your premium headphones experience by reproducing 
+                            the balanced depth and precision of studio-quality sound.
+                        </p>
+                        <button className="add-to-cart">add to cart</button>
+                    </div>
+            </div>
+        </div>
+    </body>
+  )
+}
