e4d5e8383ca2ba26c03dee9d51886d55c36794bf
NightWing2099 <62215488+KidFlash2099@users.noreply.github.com>
2023-11-02 16:23:13 -0400

    adding styles for featured and in the box section

--- audio/src/app/product-details-headphones/page.js
+++ audio/src/app/product-details-headphones/page.js
@@ -31,6 +31,37 @@ export default function details() {
                             <button className="add">+</button>
                         </div>
                     </div>
+                    <div className="in-the-box-section row">
+                        <div className="features-section">
+                            <h1>features</h1>
+                            <p className="first-paragraph">
+                                Featuring a genuine leather head strap and premium earcups, 
+                                these headphones deliver superior comfort for those who like to enjoy endless listening. 
+                                It includes intuitive controls designed for any situation. Whether you’re taking a business call or just in your own personal space, \
+                                the auto on/off and pause features ensure that you’ll never miss a beat.
+                            </p>
+
+                            <p className="second-paragraph">
+                                The advanced Active Noise Cancellation with built-in equalizer allow you to experience your audio world on your terms. 
+                                It lets you enjoy your audio in peace, 
+                                but quickly interact with your surroundings when you need to. 
+                                Combined with Bluetooth 5.0 compliant connectivity and 17 hour battery life, 
+                                the XX99 Mark II headphones gives you superior sound, cutting-edge technology, 
+                                and a modern design aesthetic.
+                            </p>
+                        </div>
+
+                        <div className="items-section">
+                        <h1>in the box</h1>
+                            <ul>
+                                <li><span>1x</span> Headphone Unit</li>
+                                <li><span>2x</span> Replacement Earcups</li>
+                                <li><span>1x</span> User Manual</li>
+                                <li><span>1x</span> 3.5mm 5m Audio Cable</li>
+                                <li><span>1x</span> Travel Bag</li>
+                            </ul>
+                        </div>
+                    </div>
             </div>
         </div>
     </body>
